300910a80ad5c83dfcf057fc662381b6c821dc31
xingjian-f <982899917@qq.com>
2016-06-30 10:59:41 +0800

    faster solution, O(n)

--- Container With Most Water.py
+++ Container With Most Water.py
@@ -1,39 +1,51 @@
 class Solution(object):
-	def binary_search(self, tar, x):
-		l = 0
-		r = len(x)
-		ret = 0
-		while l <= r:
-			mid = (l+r)/2
-			# print l, r, mid, x[mid], tar, 'bina'
-			if x[mid][0]<tar:
-				l = mid+1
-			else:
-				ret = mid
-				r = mid-1
-		return ret 
+	# def binary_search(self, tar, x):
+	# 	l = 0
+	# 	r = len(x)
+	# 	ret = 0
+	# 	while l <= r:
+	# 		mid = (l+r)/2
+	# 		# print l, r, mid, x[mid], tar, 'bina'
+	# 		if x[mid][0]<tar:
+	# 			l = mid+1
+	# 		else:
+	# 			ret = mid
+	# 			r = mid-1
+	# 	return ret 
 
-	def get_ans(self, height):
-		queue = [(height[0], 0)]
-		ans = 0
-		# print height
-		for i in range(1, len(height)):
-			if height[i] > queue[-1][0]:
-				queue.append((height[i], i))
-			else:
-				farest_id = self.binary_search(height[i], queue)
-				# print queue, height[i], farest_id
-				farest_pos = queue[farest_id][1]
-				# print height[i], i, farest_pos
-				ans = max(ans, (i-farest_pos)*height[i])
-		# print ans
-		return ans
+	# def get_ans(self, height):
+	# 	queue = [(height[0], 0)]
+	# 	ans = 0
+	# 	# print height
+	# 	for i in range(1, len(height)):
+	# 		if height[i] > queue[-1][0]:
+	# 			queue.append((height[i], i))
+	# 		else:
+	# 			farest_id = self.binary_search(height[i], queue)
+	# 			# print queue, height[i], farest_id
+	# 			farest_pos = queue[farest_id][1]
+	# 			# print height[i], i, farest_pos
+	# 			ans = max(ans, (i-farest_pos)*height[i])
+	# 	# print ans
+	# 	return ans
 
+	# def maxArea(self, height):
+	# 	"""
+	# 	:type height: List[int]
+	# 	:rtype: int
+	# 	"""
+	# 	return max(self.get_ans(height[::-1]), self.get_ans(height))
 	def maxArea(self, height):
-		"""
-		:type height: List[int]
-		:rtype: int
-		"""
-		return max(self.get_ans(height[::-1]), self.get_ans(height))
+		posl = 0
+		posr = len(height)-1
+		ans = 0
+		while posl < posr:
+			ans = max(ans, min(height[posl], height[posr])*(posr-posl))
+			if height[posl] <= height[posr]:
+				posl += 1
+			else:
+				posr -= 1
+		print ans
+		return ans
 
 Solution().maxArea([423,4342,5324,1,34,432,423,523,3])
--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Leetcode-solution
-Solutions for leetcode problems
+Solutions for leetcode problems.
 A brief description of the solutions:http://blog.csdn.net/wiking__acm/article/details/51721278
