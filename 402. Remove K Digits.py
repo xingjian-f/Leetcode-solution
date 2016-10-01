ececeedec88f9cc4db32e26c5d8aac3116cfd2ea
wiking <feixingjian@geetest.com>
2016-10-01 11:24:01 +0800

    AC

--- 402. Remove K Digits.py
+++ 402. Remove K Digits.py
@@ -5,4 +5,24 @@ class Solution(object):
         :type k: int
         :rtype: str
         """
-        
+        ret = []
+        i = 0
+        while i < len(num):
+        	min_digit = num[i]
+        	pos = i
+        	for j in range(1, k+1):
+        		if i+j < len(num) and num[i+j] < min_digit:
+        			min_digit = num[i+j]
+        			pos = i+j
+        	ret.append(min_digit)
+        	k -= pos-i
+        	i = pos + 1
+        # print ret, k
+        if k >= 1:
+        	ret = ret[:-k]
+        # print ret
+        if len(ret) == 0:
+        	return '0'
+        return str(int(''.join(ret)))
+
+print Solution().removeKdigits('042434', 1)
--- 406. Queue Reconstruction by Height.py
+++ 406. Queue Reconstruction by Height.py
@@ -4,4 +4,29 @@ class Solution(object):
         :type people: List[List[int]]
         :rtype: List[List[int]]
         """
-        
+        ret = []
+        pq = [(people[i][1], people[i][0], i) for i in range(len(people))]
+        for _ in range(len(people)):
+        	# print pq 
+        	front, val, idx = 1e9, None, None
+        	for i in pq:
+        		if i[0] < front:
+        			front, val, idx = i 
+        		elif i[0] == front and i[1] < val:
+        			front, val, idx = i 
+        	# print front, val, idx 
+        	ret.append(people[idx])
+        	tem_pq = []
+        	for i in pq:
+        		tem_front, tem_val, tem_idx = i 
+        		if idx == tem_idx:
+        			continue
+        		# print tem_val, val, idx not in vis
+        		if tem_val <= val:
+        			tem_front -= 1
+        			# print tem_front, tem_val, tem_idx
+        		tem_pq.append((tem_front, tem_val, tem_idx))
+        	pq = tem_pq
+        return ret 
+
+# print Solution().reconstructQueue([[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]])
